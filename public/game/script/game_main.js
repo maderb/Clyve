9c83137425710b9c99e27783a8f5f7b1d52933b2
Bradley Mader <Bradley Mader>
2016-07-16 17:07:42 -0700

    Added robot classes and robotCreate function, towers and towerCreate function, and notes to game_main.js file.

--- public/game/script/game_main.js
+++ public/game/script/game_main.js
@@ -1,9 +1,81 @@
 $(document).ready(function(){
-	var moveXIncrement=.6;
+	//horizontal speed and adjusted vertical speed according to aspect ratio.
+	var moveXIncrement=.4;
 	var moveYIncrement=moveXIncrement*($("#game_panel").width() / $("#game_panel").height());
+	
+	//tracks current status of directional buttons.
 	var leftMove=false,rightMove=false,upMove=false,downMove=false;
+	
+	//tracks player's x and y position.
 	var x=50,y=50;
+	
+	//DEFINE ROBOT LIST
+	var robots={
+		straightRobots:[],
+		disarmRobots:[],
+		zigRobots:[]
+	}
+	
+	//Constructor for straightRobot class
+	function straightRobot(){
+		this.name="straightRobot";
+	}
+	
+	//constructor for disarmRobot class
+	function disarmRobot(){
+		this.name="disarmRobot";
+	}
+	
+	//constructor for zigRobot class
+	function zigRobot(){
+		this.name="zigRobot";
+	}
+	
+	//Function for creating robots. Takes string containing type to be created and returns status=0 if failed.
+	function createRobot(typeName){
+		switch(typeName){
+			case "straightRobot":
+				robots.straightRobots+=new straightRobot();
+				return true;
+			case "disarmRobot":
+				robots.disarmRobots+=new disarmRobot();
+				return true;
+			case "zigRobot":
+				robots.zigRobots+=zigRobot;
+				return true;
+		}
+		return false;
+	}
+	
+	//DEFINE TOWERS
+	var towers={
+		mines:[],
+		gunTowers:[],
+		flameTowers:[]
+	}
+	
+	//Hub function for creation of new towers. Takes argument of string type and returns status 1 if valid input.
+	function createTower(typeName){
+		switch(typeName){
+			case "mine":
+				
+				return true;
+			case "gunTower":
+				
+				return true;
+			case "flameTower":
+	
+				return true;
+		}
+		return false;
+	}
 
+	//GAME LOOP: REFRESH RATE 33hz
+	setInterval(function(){
+		playerMove();
+		robotMove();
+	},30);
+	
 	//Listens for keys to be pressed and routes to appropriate function on keypress
 	document.addEventListener('keypress',function(e){
 			var key=e.which||e.keyCode;
@@ -21,25 +93,24 @@ $(document).ready(function(){
 					upMove=true;
 					break;
 				case 49:
-					console.log("ONE");
+					createRobot("straightRobot");
 					break;
 				case 50:
-					console.log("TWO");
+					createRobot("disarmRobot");
 					break;
 				case 51:
-					console.log("THREE");
+					createRobot("zigRobot");
 					break;
 				case 52:
-					console.log("FOUR");
 					break;
 				case 53:
-					console.log("FIVE");
 					break;
 				case 54:
-					console.log("SIX");
+					break;
 			}
 	});
 
+	//Set directional indicators back to false if button is lifted.
 	document.addEventListener('keyup',function(e){
 		var key=e.which||e.keyCode;
 		switch(key){
@@ -55,28 +126,11 @@ $(document).ready(function(){
 			case 38:
 				upMove=false;
 				break;
-			case 49:
-				console.log("ONE");
-				break;
-			case 50:
-				console.log("TWO");
-				break;
-			case 51:
-				console.log("THREE");
-				break;
-			case 52:
-				console.log("FOUR");
-				break;
-			case 53:
-				console.log("FIVE");
-				break;
-			case 54:
-				console.log("SIX");
 		}
 	});
-
-	setInterval(function(){
-		
+	
+	//Updates player object position
+	function playerMove(){
 		if(document.getElementById("player")){
 			if(x<99 && leftMove==false && rightMove==true)
 				x+=moveXIncrement;
@@ -91,5 +145,19 @@ $(document).ready(function(){
 			document.getElementById("player").style.left=x+"%";
 			document.getElementById("player").style.top=y+"%";
 		}
-	},30);
+	}
+	
+	//Move all robots according to specified AI functions.
+	function robotMove(){
+		for(var i=0;i<robots.straightRobots.length;i++){
+			//add movement algorithm here for straightRobots
+		}
+		for(var i=0;i<robots.disarmRobots.length;i++){
+			//add movement information here for disarmRobots
+		}
+		for(var i=0;i<robots.zigRobots.length;i++){
+			//add movement information here for zigRobots
+		}
+	}
+	
 });
